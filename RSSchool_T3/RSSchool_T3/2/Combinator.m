f765688528b5a0c5eedad30a725de877fc559273
iMintDiamond <i.mint.diamond@gmail.com>
2020-04-05 14:27:06 +0300

    feat: task2 resolve

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,24 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    long m = [array[0] longValue];
+    long n = [array[1] longValue];
+    long factN = [self factorial:n];
+    
+    for (long i = 1; i < n; i++) {
+        if (factN / ([self factorial:i] * [self factorial:(n - i)]) == m ) {
+            return [[NSNumber alloc] initWithLong:i];
+        }
+    }
+    
+    return nil;
+}
+
+- (long)factorial:(long)n {
+    if (n == 0) {
+        return 1;
+    }
+    
+    return n * [self factorial:n - 1];
 }
 @end
